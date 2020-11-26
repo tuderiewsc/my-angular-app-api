e6a081a758c9c6b223dd13b2d16ae57965d6dc0e
Ali <wizard2070@gmail.com>
2020-11-26 08:51:47 +0330

    begin secure userid in cookie

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,10 +21,43 @@ class UserController extends Controller
         }
 
         if (\Auth::validate(['email' => request('email'), 'password' => request('password')])) {
+            $user = User::where('email', request('email'))->first();
+            $id = $user->id;
+            $encrypted_id = $this->encrypt_decrypt('encrypt', $id);
+
+            $info = array(
+                'uid' => $encrypted_id
+            );
+
+            // setcookie("userToken", serialize($info), time()+1800 , '/', '' , true , true );  /* expire in 30 mins */
+
+            // return response()->json([
+            //     'token_name' => 'userToken',
+            //     'token_value' => serialize($info),
+            //     'token_type' => 'Bearer'
+            // ]);
+
             return User::where('email', request('email'))->first();
         }
 
         return response(['status' => 'error', 'message' => 'password or email not validate'], 404);
+    }
+
+    function encrypt_decrypt($action, $string) {
+        $output = false;
+        $encrypt_method = "AES-256-CBC";
+        $secret_key = 'H+MbQeThWmYq3t6w9z$C&F)J@NcRfUjXn2r5u7x!A%D*G-KaPdSgVkYp3s6v9y/B';
+        $secret_iv = 'RfUjXn2r5u8x/A?D(G-KaPdSgVkYp3s6v9y$B&E)H@MbQeThWmZq4t7w!z%C*F-J';
+        $key = hash('sha256', $secret_key);
+    // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+        $iv = substr(hash('sha256', $secret_iv), 0, 16);
+        if ( $action == 'encrypt' ) {
+            $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+            $output = base64_encode($output);
+        } else if( $action == 'decrypt' ) {
+            $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+        }
+        return $output;
     }
 
     public function register()
@@ -32,7 +65,7 @@ class UserController extends Controller
         $validator = Validator::make(request()->all(), [
             'name' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
-           'password' => 'required|min:6|confirmed'
+            'password' => 'required|min:6|confirmed'
         ]);
 
         if ($validator->fails()) {
@@ -66,20 +99,20 @@ class UserController extends Controller
         }
 
     }
-	
+
     public function get_user($id){
         $user = User::find($id);
-		return $user;
+        return $user;
     }
-	
+
     public function auth_api(Request $request){
-		return $request->user();
-    }
-	
-	
-	
-	
-	
-	
-	
+      return $request->user();
+  }
+
+
+
+
+
+
+
 }
